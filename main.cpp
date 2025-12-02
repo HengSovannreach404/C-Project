6ede7e63ad75e7642189145204a746dec218a708
HengSovannreach404 <sovannreach.heng@student.cadt.edu.kh>
2025-12-02 21:32:51 +0700

    push

--- main.cpp
+++ main.cpp
@@ -15,20 +15,24 @@ class playlist {
 private:
     Node *head;
     Node *current;
+    Node *Prev;
 public:
     playlist(){
         head = nullptr;
         current = nullptr;
+        Prev = nullptr;
     }
     void addSong (string name){
         Node *NewSong = new Node(name);
         if( head == nullptr){
             head = NewSong;
             current = NewSong;
+            Prev = NewSong;
         } else {
             Node* temp = head;
-            while (temp->next != nullptr)
+            while (temp->next != nullptr){
                 temp = temp->next;
+            }
             temp->next = NewSong;
         }
         cout << "Added :" << name <<endl;
@@ -41,7 +45,7 @@ public:
             Node *temp = head;
             while (temp != nullptr){
                 if ( temp == current){
-                    cout<< ">"<<endl;
+                    cout<< "> ";
                 } else {
                     cout << "  " <<endl;
                 }
@@ -50,10 +54,63 @@ public:
             }
         }
     }
+    void play() {
+        if (current == nullptr) {
+            cout << "No songs to play.\n";
+            return;
+        }
+        cout << "Playing: " << current->title << endl;
+    }
+    void playnext (){
+        if(current == nullptr){
+            cout << "NO songs in playlist."<<endl;
+            return;
+        } else  if (current->next == nullptr){
+            cout<< "This is the last song."<<endl;
+            return;
+        }
+        current = current->next;
+        play();
+    }
+
 };
 int main () {
-    playlist p1;
-    p1.addSong(papaN);
-    p1.display();
+    playlist p;
+    int option;
+    string name ;
+    do{
+        cout<< "\n--- Music Player Menu ---" << endl;
+        cout<< "1. Add Song"<< endl;
+        cout<< "2. Show Playlist"<< endl;
+        cout<< "3. Play Music"<< endl;
+        cout<< "4. Next Music"<< endl;
+        cout<< "0. exit program"<< endl;
+        cout<< "Choose Option : ";
+        cin >> option ;
+        switch (option){
+            case 1 : 
+                cout << "Enter A Song : " ;
+                cin.ignore();
+                getline(cin, name);
+                p.addSong(name);
+                break;
+            case 2 : 
+                p.display();
+                break;
+            case 3 :
+                p.play();
+                break;
+            case 4 :
+                p.playnext();
+                break; 
+            case 0: 
+                cout<<"Exit program"<<endl;
+            
+            
+        }
+
+    }while(option!=0);
+
+    return 0 ;
 
 }
